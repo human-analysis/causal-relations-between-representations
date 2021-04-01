a8aa6a7c03aa1e7641114aaa103175a16d5378ec
lan-lw <wanglan3@msu.edu>
2021-04-01 13:42:37 -0400

    add causalgenerator

--- crl/main.py
+++ crl/main.py
@@ -1,2 +1,8 @@
 # main.py
+from causalgenerator import CausalGenerator
 
+nsamples=1000
+ndims=8
+nranges=2
+generator=CausalGenerator('cubicspline')
+data=generator.generator(nsamples,ndims,nranges)
